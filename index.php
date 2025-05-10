a1a09d7493ddd91ad77b27455c2af7c90f3a9546
gluglu553 <hugoomartins2005@outlook.com>
2025-05-10 17:48:22 -0300

    correcao,barraLateral

--- index.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Caminho base dinâmico
-$base = rtrim(dirname($_SERVER['SCRIPT_NAME']), '/');
-
-// Redireciona para a tela de login
-header("Location: $base/src/app/public/frontend/View/login.php");
-exit;
-?>
