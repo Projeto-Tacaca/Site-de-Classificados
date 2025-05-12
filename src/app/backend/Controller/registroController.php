a5cec92537ac2613a74d067599b701a33f3d6a66
Gaia28 <matheusgaia33@gmail.com>
2025-05-11 21:20:35 -0300

    Verificação de email na tela de cadastro

--- src/app/backend/Controller/registroController.php
+++ src/app/backend/Controller/registroController.php
@@ -12,9 +12,18 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST'){
     $senha = $_POST['senha'];
     $confirmSenha = $_POST['confirmSenha'];
 
-    $querySQL = "SELECT 1 FROM usuarios WHERE email = '$email'";
-    
+    $querySQL = "SELECT 1 FROM usuarios WHERE email = '?'";
 
+    $preparedStatment = $connection->prepare($querySQL);
+    $preparedStatment->bind_param("s", $email);
+    $result = $preparedStatment->execute();
+
+    if($result->num_rows > 0){
+        echo "<script>alert('E-mail já cadastrado.');</script>";
+        echo "<script>window.location.href = '../../public/frontend/View/registro.php';</script>";
+        exit();
+    }
+    
     if($senha != $confirmSenha){
         echo "<script>alert('As senhas não coincidem.');</script>";
         echo "<script>window.location.href = '../../public/frontend/View/registro.php';</script>";
