47f5e43b499b473da3caa476044a5781dd91d046
kemmis13 <kemmilyriany@gmail.com>
2025-05-13 16:24:13 -0300

    arrumei a imagem

---
+++ src/app/public/frontend/View/anuncio.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Anúncio</title>
+  <link rel="stylesheet" href="../css/style.css">
+</head>
+<body>
+
+  <div class="header">
+    <span class="arrow">&#8592;</span>
+  <span class = "header-text"> VER-O-ANÚNCIO </span>
+  <link rel="stylesheet" href="../css/anuncio.css">
+  </div>
+
+  <div class="container">
+    <div class="card">
+      <div class="image-box">
+        <img src="https://via.placeholder.com/250x200?text=foto" alt="foto do serviço">
+        <span class="star">&#9733;</span>
+      </div>
+
+      <div class="content">
+        <h2>Serviços domésticos</h2>
+        <p>
+          descrição do produto
+        </p>
+        <h3>R$00,00</h3>
+        <p class="seller"><strong>Vendedor:</strong> seu nome</p>
+        <p class="address">endereço</p>
+        <a class="btn" href="#">Falar com vendedor</a>
+      </div>
+    </div>
+  </div>
+
+</body>
+</html>
