e9d31ae105b25a99a1faf3819cd40340b9d26424
Gaia28 <matheusgaia33@gmail.com>
2025-05-12 23:04:25 -0300

    Validação dos formulários de login e cadastro

--- src/app/backend/Controller/registroController.php
+++ src/app/backend/Controller/registroController.php
@@ -2,8 +2,6 @@
 require '../../../../database/config.php';
 
 
-
-
 if ($_SERVER['REQUEST_METHOD'] == 'POST'){
 
     $nome = $_POST['nome'];
@@ -12,26 +10,44 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST'){
     $senha = $_POST['senha'];
     $confirmSenha = $_POST['confirmSenha'];
 
-    $querySQL = "SELECT 1 FROM usuarios WHERE email = '?'";
+    // Verifica se o e-mail já está cadastrado
+    $querySQL = "SELECT 1 FROM usuarios WHERE email = ?";
 
     $preparedStatment = $connection->prepare($querySQL);
     $preparedStatment->bind_param("s", $email);
-    $result = $preparedStatment->execute();
+    $preparedStatment->execute();
+    $preparedStatment->store_result();
 
-    if($result->num_rows > 0){
+    if($preparedStatment->num_rows > 0){
         echo "<script>alert('E-mail já cadastrado.');</script>";
         echo "<script>window.location.href = '../../public/frontend/View/registro.php';</script>";
+        $preparedStatment->close();
         exit();
     }
-    
+    $preparedStatment->close();
+
+    // Verifica se a senha e a confirmação de senha são iguais
     if($senha != $confirmSenha){
         echo "<script>alert('As senhas não coincidem.');</script>";
         echo "<script>window.location.href = '../../public/frontend/View/registro.php';</script>";
         exit();
 
+    }else{
+        // Criptografa a senha
+        $senha = password_hash($senha, PASSWORD_DEFAULT);
+
+        // Insere os dados do usuário no banco de dados
+        $querySQL = "INSERT INTO usuarios (nome, email, senha, telefone) VALUES (?, ?, ?, ?)";
+        $preparedStatment = $connection->prepare($querySQL);
+        $preparedStatment->bind_param("ssss", $nome, $email, $senha, $telefone);    
+        $preparedStatment->execute();
+        $preparedStatment->close();
+
+        // Redireciona para a tela de login
+        echo "<script>alert('Usuário cadastrado com sucesso!');</script>";
+        echo "<script>window.location.href = '../../public/frontend/View/login.php';</script>";
     }
 
-    
 }
 
 
