df75c4b52037917cb34298a641f7f020203feb2a
kemmis13 <kemmilyriany@gmail.com>
2025-05-20 20:42:37 -0300

    pubblicaçao

---
+++ src/app/public/frontend/View/piblicaranuncio.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Seu Anúncio</title>
+    <link rel="stylesheet" href="../css/piblicaranuncio.css">
+</head>
+<body>
+    <div class="header">
+        <h2>Seu anúncio</h2>
+    </div>
+    <div class="container">
+        <div class="photo">foto</div>
+        <div class="indicator">
+            <div class="active"></div>
+            <div></div>
+            <div></div>
+        </div>
+        <div class="form-group">
+            <label>Título do anúncio</label>
+            <input type="text" placeholder="Título" />
+        </div>
+        <div class="form-group">
+            <label>Descrição</label>
+            <textarea placeholder="Descrição"></textarea>
+        </div>
+        <div class="form-group">
+            <label>Valor R$</label>
+            <input type="text" placeholder="Valor" />
+        </div>
+        <div class="form-group">
+            <label>Endereço</label>
+            <input type="text" placeholder="Endereço" />
+        </div>
+        <button>Concluir</button>
+    </div>
+</body>
+</html>
